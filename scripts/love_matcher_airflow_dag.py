3a6a239cf08adbbf8afec3005349207691ff559c
Sandra <spietrowska@xebia.fr>
2017-04-16 18:46:17 +0200

    added eval and predict dag to airflow

--- scripts/love_matcher_airflow_dag.py
+++ scripts/love_matcher_airflow_dag.py
@@ -2,11 +2,14 @@ import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-from love_matcher.refactored.airflow.task_classes import FeatureEngineeringTask, TuneTask, TrainTask
+from love_matcher.refactored.airflow.task_classes import FeatureEngineeringTask, TuneTask, TrainTask, EvalTask, PredictTask
+from docs import *
+
+model_type = "Decision_Tree"
 
 args = {
     'owner': 'Sandoine',
-    'start_date': airflow.utils.dates.days_ago(2)
+    'start_date': airflow.utils.dates.days_ago(2),
 }
 
 dag = DAG(
@@ -20,13 +23,24 @@ feature_engineering = PythonOperator(
 
 tune = PythonOperator(
     task_id='love_matcher_tune',
-    python_callable=TuneTask().run,
+    python_callable=TuneTask(model_type=model_type).run,
     dag=dag)
 
 train = PythonOperator(
     task_id='love_matcher_train',
-    python_callable=TrainTask().run,
+    python_callable=TrainTask(model_type=model_type).run,
     dag=dag)
 
+eval = PythonOperator(
+    task_id='love_matcher_eval',
+    python_callable=EvalTask(model_type=model_type).run,
+    dag=dag)
+
+predict = PythonOperator(
+    task_id='love_matcher_predict',
+    python_callable=PredictTask(model_type=model_type).run,
+    dag=dag)
+
+predict.set_upstream(train)
 train.set_upstream(tune)
 tune.set_upstream(feature_engineering)
