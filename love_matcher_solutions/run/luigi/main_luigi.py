7917f877f0fc8dd8acd608f93f9918ba3293a7a9
Sandra <spietrowska@xebia.fr>
2017-05-05 23:41:49 +0200

    added exercices and solutions for luigi part

--- love_matcher_solutions/run/luigi/main_luigi.py
+++ love_matcher_solutions/run/luigi/main_luigi.py
@@ -26,9 +26,9 @@ class FeatureEngineeringTask(luigi.Task):
 
         # Feature engineering
         feature_engineering = FeatureEngineering(features=features)
-        feat_eng_df, processed_features_names = feature_engineering.get_partner_features(dataset_df)
-        feat_eng_df.to_csv(feature_engineered_dataset_file_path, index=False)
-        processed_features_names.to_csv(processed_features_names_file_path, index=False)
+        all_features_engineered_df, selected_features_df = feature_engineering.get_partner_features(dataset_df)
+        all_features_engineered_df.to_csv(feature_engineered_dataset_file_path, index=False)
+        selected_features_df.to_csv(processed_features_names_file_path, index=False)
 
     def read_dataframe(self):
         return pd.read_csv(workspace + "Speed_Dating_Data.csv", encoding="ISO-8859-1")
@@ -41,33 +41,27 @@ class TrainTask(luigi.Task):
         return luigi.LocalTarget(output_dir + '/' + str(self.model_type) + '_model.pkl')
 
     def requires(self):
-        if self.model_type == "Decision_Tree":
-            self.start_time = datetime.datetime.now()
-            return FeatureEngineeringTask()
-        else:
-            pass
+        # TODO: Solution 5.1
+        return FeatureEngineeringTask()
 
     def run(self):
         feat_eng_df = pd.read_csv(feature_engineered_dataset_file_path)
         processed_features_names_df = pd.read_csv(processed_features_names_file_path)
+
         split_test_train = SplitTestTrain(feat_eng_df=feat_eng_df, processed_features_names=processed_features_names_df)
         x_train, x_test, y_train, y_test = split_test_train.create_train_test_splits()
+
         train = Trainer(x_train, y_train, x_test, y_test, model_type=str(self.model_type))
         train.save_estimator(output_dir)
         estimator = train.combiner_pipeline()
         print(estimator)
-        end_time = datetime.datetime.now()
-        print("Total time spent %s" % (end_time - self.start_time))
-
-    def load_best_parameters(self):
-        with open(output_dir + "/" + str(self.model_type) + "_best_parameters.json") as best_parameters_file:
-            return json.load(best_parameters_file)
 
 
 class EvaluationTask(luigi.Task):
     model_type = luigi.Parameter()
 
     def output(self):
+        # TODO: Solution 5.2
         return luigi.LocalTarget(output_dir + "/" + str(self.model_type) + "_eval.txt")
 
     def requires(self):
@@ -76,8 +70,10 @@ class EvaluationTask(luigi.Task):
     def run(self):
         feat_eng_df = pd.read_csv(feature_engineered_dataset_file_path)
         processed_features_names_df = pd.read_csv(processed_features_names_file_path)
+
         split_test_train = SplitTestTrain(feat_eng_df=feat_eng_df, processed_features_names=processed_features_names_df)
         x_train, x_test, y_train, y_test = split_test_train.create_train_test_splits()
+
         evaluation = Evaluator(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test,
                                model_type=str(self.model_type))
         evaluation.eval()
@@ -90,6 +86,7 @@ class PredictionsTask(luigi.Task):
         return luigi.LocalTarget(output_dir + "/" + str(self.model_type) + "_predictions.csv")
 
     def requires(self):
+        # TODO: Solution 5.3
         return TrainTask(self.model_type)
 
     def run(self):
@@ -109,4 +106,4 @@ class GeneratePredictions(luigi.WrapperTask):
                PredictionsTask(model_type=self.model_type)
 
 
-# TODO : luigi --module love_matcher_exercices.run.luigi.main_luigi GeneratePredictions
+# TODO : luigi --module love_matcher_solutions.run.luigi.main_luigi GeneratePredictions
